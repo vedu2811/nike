a047c895a53a24b3980e9f5644bd3aaac5480729
Vedant <vedant28j@gmail.com>
2025-07-19 18:06:40 +0000

    Created different sections for the App

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <main className='relative'>
+      Nav
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        Hero
+      </section>
+      <section className='padding'>
+        Popular Products
+      </section>
+      <section className='padding'>
+        SuperQuality
+      </section>
+      <section className='padding-x py-10' >
+        Services
+      </section>
+      <section className='padding'>
+        Special Offers
+      </section>
+      <section className='padding bg-pale-blue'>
+        Customer Reviews
+      </section>
+      <section className='padding padding-x sm:py-32 py-16 w-full'>
+        Subscribe
+      </section>
+      <section className='bg-black padding-x padding-t pb-8'>
+        Footer
+      </section>
+    </main>
+  );
 }
 
 export default App
