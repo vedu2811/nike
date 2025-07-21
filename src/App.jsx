51cdd14f204aa732a54a443169598c72168aa733
Vedant <vedant28j@gmail.com>
2025-07-21 19:22:52 +0000

    Importing index section

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
+import { CustomerReviews,Footer,Hero,PopularProducts,Services,SpecialOffer,Subscribe,SuperQuality } from "./sections";
 
 const App = () => {
   return (
     <main className='relative'>
-      Nav
+      <Nav />
       <section className='xl:padding-l wide:padding-r padding-b'>
-        Hero
+        <Hero />
       </section>
       <section className='padding'>
-        Popular Products
+        <PopularProducts />
       </section>
       <section className='padding'>
-        SuperQuality
+        <SuperQuality />
       </section>
       <section className='padding-x py-10' >
-        Services
+        <Services />
       </section>
       <section className='padding'>
-        Special Offers
+        <SpecialOffer />
       </section>
       <section className='padding bg-pale-blue'>
-        Customer Reviews
+        <CustomerReviews />
       </section>
       <section className='padding padding-x sm:py-32 py-16 w-full'>
-        Subscribe
+        <Subscribe />
       </section>
       <section className='bg-black padding-x padding-t pb-8'>
-        Footer
+        <Footer />
       </section>
     </main>
   );
