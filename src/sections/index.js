b994207483164794f7e06e44d7eee9ade7c33aca
Vedant <vedant28j@gmail.com>
2025-07-21 19:18:57 +0000

    Index file for creating an one line import in App.jsx

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from './SuperQuality';
+
+export{
+    Hero,
+    PopularProducts,
+    Footer,
+    CustomerReviews,
+    Subscribe,
+    SuperQuality,
+    Services,
+    SpecialOffer
+}
