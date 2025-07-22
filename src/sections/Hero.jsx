71cede61de494181b4cd753603ea17b64bc58a53
Vedant <vedant28j@gmail.com>
2025-07-22 11:32:13 +0000

    css for third heading

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
           
         </h1>
-        <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
         <Button label="Shop Now" iconURL={arrowRight} />
 
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
