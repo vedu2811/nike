7f345adf5c4f96a8e74132e32b4d63ba55dcddb5
Vedant <vedant28j@gmail.com>
2025-07-22 11:15:24 +0000

    CSS for the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 
-const Button = () => {
+const Button = ({label,iconURL}) => {
   return (
-    <button className="">Click Me</button>
+    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+        {label}
+        <img
+            src={iconURL}
+            alt="Arrow Right"
+            className="ml-2 rounded-full w-5 h-5"
+        />
+    </button>
   )
 }
 
