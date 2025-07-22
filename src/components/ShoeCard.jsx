97b0e8b703f020d1d5bd1e1000b57b775709fec1
Vedant <vedant28j@gmail.com>
2025-07-22 12:13:57 +0000

    Error: imgURL.bigShoe

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -8,7 +8,7 @@ const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImg}) => {
     }
 
   return (
-    <div className={`border-2 rounded-xl ${bigShoeImg===imgURL ? 'border-coral-red' : 'border-transparent'} cursor-pointer max-sm:flex-1`}
+    <div className={`border-2 rounded-xl ${bigShoeImg===imgURL.bigShoe ? 'border-coral-red' : 'border-transparent'} cursor-pointer max-sm:flex-1`}
         onClick={handleClick}
     >
         <div className="flex justify-center items-center bg-card bg-center bg-cover sm:h-40 sm:w-40 rounded-xl max-sm:p-4">
