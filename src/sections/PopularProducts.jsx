e46c04a50ecc93229ef21d9fe753aa05ca185900
Vedant <vedant28j@gmail.com>
2025-07-22 16:59:21 +0000

    PopularProducts section: heading and description

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,10 +1,13 @@
-
+import { products } from "../constants"
 
 const PopularProducts = () => {
   return (
-    <div>
-        
-    </div>
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">Our <span className="text-coral-red">Popular</span> Products</h2>
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">Experience top-notch quality with our sought-after selections. Discover a world of comfort, design, and value</p>
+      </div>
+    </section>
   )
 }
 
