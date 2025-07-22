bc317da52b6c1f231b9349a84ac322e20130e86b
Vedant <vedant28j@gmail.com>
2025-07-22 11:23:00 +0000

    Css for the first heading

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p>Our Summer Collection</p>
+        <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
         <h1>
           <span>The New Arrival</span>
           <br />
