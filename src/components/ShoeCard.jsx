ad1d7230053ffb5184e3ec0c974943a3ce51bfb0
Vedant <vedant28j@gmail.com>
2025-07-22 12:02:31 +0000

    Adding the shoe images in the shoe card

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -11,7 +11,15 @@ const ShoeCard = ({imgURL, changeBigShoeImage, bigShoeImg}) => {
     <div className={`border-2 rounded-xl ${bigShoeImg===imgURL ? 'border-coral-red' : 'border-transparent'} cursor-pointer max-sm:flex-1`}
         onClick={handleClick}
     >
-
+        <div className="flex justify-center items-center bg-card bg-center bg-cover sm:h-40 sm:w-40 rounded-xl max-sm:p-4">
+            <img
+                src={imgURL.thumbnail}
+                alt="Shoe Colleaction"
+                width={127}
+                height={103}
+                className="object-contain"
+            />
+        </div>
     </div>
   )
 }
