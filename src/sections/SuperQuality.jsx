7202517942b795df62c5b5f16f26aad4c0f6992c
Vedant <vedant28j@gmail.com>
2025-07-23 21:14:13 +0000

    Super Quality section (all the left side)

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,3 +1,4 @@
+import Button from "../components/Button"
 
 const SuperQuality = () => {
   return (
@@ -6,7 +7,15 @@ const SuperQuality = () => {
       className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
     >
       <div className="flex flex-1 flex-col">
-        
+        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You <span className="text-coral-red">Super Quality</span> Shoes
+          <br />
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">Ensuring premium confort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
+        <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction</p>
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
       </div>
     </section>
   )
