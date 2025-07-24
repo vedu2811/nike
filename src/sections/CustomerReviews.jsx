ae656e66aef422724e6dd0f5d8c6472cda1e47fa
Vedant <vedant28j@gmail.com>
2025-07-24 23:38:08 +0000

    mapping the customer card to the review array in the index file under constants

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -12,7 +12,13 @@ const CustomerReviews = () => {
 
       <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
         {reviews.map((review)=>(
-          <ReviewCard />
+          <ReviewCard 
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
         ))}
       </div>
     </section>
