3ac5118287eb2b0a548d7c330d18569c70ed8b75
Vedant <vedant28j@gmail.com>
2025-07-24 23:49:00 +0000

    Subscribe section started

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,9 @@
 
 const Subscribe = () => {
   return (
-    <div></div>
+    <section className="max-container flex justify-between items-center max-lg:flex-col gap-10" id="contact-us">
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign up for <span className="text-coral-red">Updates </span>& Newsletter</h3>
+    </section>
   )
 }
 
