53a64fe8990a67cd879b4cf0cbf627bb1c1f5158
Vedant <vedant28j@gmail.com>
2025-07-24 23:29:29 +0000

    Info text

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -6,6 +6,9 @@ const CustomerReviews = () => {
         What Our
         <span className="text-coral-red"> Customers</span> Say?
       </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
+
+      
     </section>
   )
 }
