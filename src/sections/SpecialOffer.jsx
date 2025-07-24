be8013451e66c7c4f8b0d15fca7224ed54d68c5f
Vedant <vedant28j@gmail.com>
2025-07-24 23:01:43 +0000

    Making modifications to the imported code

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,3 +1,4 @@
+import { arrowRight } from "../assets/icons"
 import { offer } from "../assets/images"
 import Button from "../components/Button"
 
@@ -9,13 +10,14 @@ const SpecialOffer = () => {
        </div>
        <div className="flex flex-1 flex-col">
         <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
-          We Provide You <span className="text-coral-red">Super Quality</span> Shoes
+          <span className="text-coral-red">Special</span> Offer
           <br />
         </h2>
-        <p className="mt-4 lg:max-w-lg info-text">Ensuring premium confort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
-        <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction</p>
-        <div className="mt-11">
-          <Button label="View details" />
+        <p className="mt-4 lg:max-w-lg info-text">Embark on a shopping journey that redefines your experience with unbeatable deals. From premier selections to incredible savings, we offer unparalleled value that sets us apart.</p>
+        <p className="mt-6 lg:max-w-lg info-text">Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.</p>
+        <div className="mt-11 flex gap-10">
+          <Button label="Shop now" iconURL={arrowRight}/>
+          {/* <Button label="Learn more" /> */}
         </div>
       </div>
     </section>
