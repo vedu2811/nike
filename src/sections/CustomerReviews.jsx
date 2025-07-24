3f9f276025cbd0847c363e4a0a280ae996d7e3a3
Vedant <vedant28j@gmail.com>
2025-07-24 23:26:14 +0000

    Customer reviews section started

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
 
 const CustomerReviews = () => {
   return (
-    <div>
-        
-    </div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red"> Customers</span> Say?
+      </h3>
+    </section>
   )
 }
 
