c1a9600edf72c11a60d8dda349caf93bf2ea387e
Vedant <vedant28j@gmail.com>
2025-07-24 23:53:11 +0000

    Adding input placeholder and button

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,8 +1,16 @@
+import Button from "../components/Button"
 
 const Subscribe = () => {
   return (
     <section className="max-container flex justify-between items-center max-lg:flex-col gap-10" id="contact-us">
       <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign up for <span className="text-coral-red">Updates </span>& Newsletter</h3>
+
+      <div>
+        <input type="text" placeholder="youremail@gmail.com" className="input" />
+        <div>
+          <Button label="Sign Up"/>
+        </div>
+      </div>
     </section>
   )
 }
