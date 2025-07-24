b10fd7c26c711fea9cecf14bcfdbc139edf2ef48
Vedant <vedant28j@gmail.com>
2025-07-24 23:04:05 +0000

    Making the two buttons for the section

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -15,9 +15,9 @@ const SpecialOffer = () => {
         </h2>
         <p className="mt-4 lg:max-w-lg info-text">Embark on a shopping journey that redefines your experience with unbeatable deals. From premier selections to incredible savings, we offer unparalleled value that sets us apart.</p>
         <p className="mt-6 lg:max-w-lg info-text">Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.</p>
-        <div className="mt-11 flex gap-10">
+        <div className="mt-11 flex flex-wrap gap-4">
           <Button label="Shop now" iconURL={arrowRight}/>
-          {/* <Button label="Learn more" /> */}
+          <Button label="Learn more" backgroundColor="bg-white" borderColor="border-slate-gray" textColor="text-slate-gray"/>
         </div>
       </div>
     </section>
