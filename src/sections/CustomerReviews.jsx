7bf97c6d802a23b763f830511c1a06b48bc80c35
Vedant <vedant28j@gmail.com>
2025-07-24 23:32:40 +0000

    Adding and importing a review card

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,3 +1,5 @@
+import { reviews } from "../constants"
+import ReviewCard from "../components/ReviewCard"
 
 const CustomerReviews = () => {
   return (
@@ -8,7 +10,11 @@ const CustomerReviews = () => {
       </h3>
       <p className="info-text m-auto mt-4 max-w-lg text-center">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
 
-      
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review)=>(
+          <ReviewCard />
+        ))}
+      </div>
     </section>
   )
 }
