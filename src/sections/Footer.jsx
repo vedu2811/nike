5ee4f8abdaf054836c33527750f6ce72d045648b
Vedant <vedant28j@gmail.com>
2025-07-25 00:19:32 +0000

    Footer section started

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,20 @@
+import {footerLogo} from '../assets/images'
+
 const Footer = () => {
   return (
-    <div>
-        
-    </div>
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href='/'>
+            <img 
+              src={footerLogo}
+              width={150}
+              height={46}
+            />
+          </a>
+        </div>
+      </div>
+    </footer>
   )
 }
 
