b83b0cdabb600f075e81562fa5b36914f319d103
Vedant <vedant28j@gmail.com>
2025-07-25 01:19:17 +0000

    Final Commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -112,7 +112,7 @@ export const footerLinks = [
         title: "Get in touch",
         links: [
             { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
+            { name: "+91XXXXX", link: "tel:+91XXXXX" },
         ],
     },
 ];
