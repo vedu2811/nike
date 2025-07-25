46b5d5936a7fde03e09855a83a6b5e74b9693d10
Vedant <vedant28j@gmail.com>
2025-07-25 01:02:56 +0000

    Adding footer description and social media icons

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,4 +1,5 @@
 import {footerLogo} from '../assets/images'
+import {socialMedia} from '../constants'
 
 const Footer = () => {
   return (
@@ -12,6 +13,19 @@ const Footer = () => {
               height={46}
             />
           </a>
+          <p className='mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm'>Get shoes ready for the new term at your nearest Nike store. Find your perfect Size In Store. Get Rewards</p>
+          <div className='flex items-center gap-5 mt-8'>
+            {socialMedia.map((icon)=>(
+              <div className='flex justify-center items-center w-12 h-12 bg-white rounded-full'>
+                <img
+                  src={icon.src}
+                  alt={icon.alt}
+                  width={24}
+                  height={24}
+                />
+                </div>
+            ))}
+          </div>
         </div>
       </div>
     </footer>
