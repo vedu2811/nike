3df0a551bdc2a53527e3db02afdd77ddeca385f2
Vedant <vedant28j@gmail.com>
2025-07-25 00:03:25 +0000

    Adding classsnames to both the div

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -5,10 +5,10 @@ const Subscribe = () => {
     <section className="max-container flex justify-between items-center max-lg:flex-col gap-10" id="contact-us">
       <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign up for <span className="text-coral-red">Updates </span>& Newsletter</h3>
 
-      <div>
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="youremail@gmail.com" className="input" />
-        <div>
-          <Button label="Sign Up"/>
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up" fullWidth/>
         </div>
       </div>
     </section>
